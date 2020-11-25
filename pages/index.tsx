a21004403bcb4a1c85f5f63b7c4c88a989e7be99
TaiNguyen <tai@useget.com>
2020-11-25 22:22:51 +0700

    typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import utilStyles from '../styles/utils.module.css';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   console.log({ allPostsData });
   return {
@@ -13,9 +14,17 @@ export async function getStaticProps() {
       allPostsData,
     },
   };
-}
+};
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  };
+}) {
   return (
     <Layout home>
       <Head>
